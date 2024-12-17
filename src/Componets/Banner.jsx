ed2c4a7bd6cd37111a712995cc8a44cf578b288f
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-12-17 14:00:28 -0600

    update Banner.jsx

--- src/Componets/Banner.jsx
+++ src/Componets/Banner.jsx
@@ -2,12 +2,14 @@ import banner from "../../public/Media/banner.jpg";
 
 const Banner = () => {
   return (
-    <div className="flex justify-center items-center mt-6">
-      <img
-        className="max-w-screen-md  rounded-3xl object-cover"
-        src={banner}
-        alt="Banner"
-      />
+    <div className="flex justify-center items-center mt-6 w-full">
+      <div className="w-full max-w-screen-md aspect-w-16 aspect-h-9 rounded-3xl overflow-hidden">
+        <img
+          className="w-full h-full object-cover"
+          src={banner}
+          alt="Banner"
+        />
+      </div>
     </div>
   );
 };
