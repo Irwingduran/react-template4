44cf99acead3974c8fbc693a6a3b13c67dd01425
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-12-17 10:40:47 -0600

    update commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,14 +5,19 @@ import Faq from "../../Componets/Faq";
 import Service from "../../Componets/Service";
 import Testimonial from "../../Componets/Testimonial";
 import About from "../../Componets/About";
-
+import MapComponent from "../../Componets/MapComponent";
+import VideoComponent from "../../Componets/VideoComponent";
+import WhatsAppButton from "../../Componets/WhatsAppButton";
 const Home = () => {
   return (
     <div>
+      <WhatsAppButton></WhatsAppButton>
       <Banner></Banner>
       <CareSection></CareSection>
       <About></About>
       <Service></Service>
+      <MapComponent></MapComponent>
+      <VideoComponent></VideoComponent>
       <Testimonial></Testimonial>
       <Faq></Faq>
       <Discount></Discount>
